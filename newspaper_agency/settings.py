a5024aa18d5a6e38823b63b44ee020d845679d4e
Kyrylo Storozhenko <busterakel@gmail.com>
2024-03-06 12:29:41 +0200

    fix incorrect application name in installed_apps

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "newspaper_agency",
+    "newspaper",
 ]
 
 MIDDLEWARE = [
