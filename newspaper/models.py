feb9fc3a55b1eb54659b0cb64bbb271145162459
Kyrylo Storozhenko <busterakel@gmail.com>
2024-03-06 12:24:21 +0200

    add model Newspaper to newspaper/models.py

--- newspaper/models.py
+++ newspaper/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from newspaper_agency import settings
+
 
 class Topic(models.Model):
     name = models.CharField(max_length=255)
@@ -17,3 +19,21 @@ class Redactor(AbstractUser):
 
     class Meta:
         ordering = ("username", )
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    publish_date = models.DateField(auto_now_add=True)
+    topic = models.ForeignKey(
+        Topic,
+        on_delete=models.CASCADE,
+        related_name="newspapers"
+    )
+    redactors = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="newspapers")
+
+    class Meta:
+        ordering = ("title", )
+
+    def __str__(self):
+        return f"{self.title} {self.publish_date} topic:{self.topic}"
