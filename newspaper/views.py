03e233e2528a02b089d63d39e3696353bd7e4c00
Kyrylo Storozhenko <busterakel@gmail.com>
2024-03-07 13:20:35 +0200

    add number visits

--- newspaper/views.py
+++ newspaper/views.py
@@ -10,10 +10,13 @@ def index(request: HttpRequest) -> HttpResponse:
     num_newspapers = Newspaper.objects.count()
     num_redactors = Redactor.objects.count()
     num_topics = Topic.objects.count()
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
     context = {
         "num_newspapers": num_newspapers,
         "num_redactors": num_redactors,
-        "num_topics": num_topics
+        "num_topics": num_topics,
+        "num_visits": num_visits + 1
     }
     return render(
         request,
@@ -33,6 +36,7 @@ class TopicDetailView(DetailView):
 
 class NewspaperListView(ListView):
     model = Newspaper
+    paginate_by = 5
 
 
 class NewspaperDetailView(DetailView):
