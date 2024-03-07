99ad465eafc164744356b0a6ff76c596b3964442
Kyrylo Storozhenko <busterakel@gmail.com>
2024-03-07 15:25:26 +0200

    implement login and logout pages

--- newspaper_agency/urls.py
+++ newspaper_agency/urls.py
@@ -18,10 +18,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
-
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("newspaper/", include("newspaper.urls", namespace="newspaper")),
+    path("", include("newspaper.urls", namespace="newspaper")),
+    path("accounts/", include("django.contrib.auth.urls")),
 ]
-
