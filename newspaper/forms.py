f06a632cdc030ca7a2cf98f099d9c08a71c5d0b6
Kyrylo Storozhenko <busterakel@gmail.com>
2024-03-07 19:33:46 +0200

    add redactor create view

---
+++ newspaper/forms.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.forms import UserCreationForm
+from newspaper.models import Redactor
+
+
+class RedactorCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = Redactor
+        fields = UserCreationForm.Meta.fields + ("first_name", "last_name", "email", "years_of_experience")
--- newspaper/urls.py
+++ newspaper/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from newspaper.views import index, TopicListView, NewspaperListView, NewspaperDetailView, RedactorListView, \
     RedactorDetailView, TopicDetailView, TopicCreateView, NewspaperCreateView, TopicUpdateView, TopicDeleteView, \
-    NewspaperUpdateView, NewspaperDeleteView
+    NewspaperUpdateView, NewspaperDeleteView, RedactorCreateView
 
 urlpatterns = [
     path("", index, name="index"),
@@ -17,7 +17,8 @@ urlpatterns = [
     path("newspapers/<int:pk>/update/", NewspaperUpdateView.as_view(), name="newspaper-update"),
     path("newspapers/<int:pk>/delete/", NewspaperDeleteView.as_view(), name="newspaper-delete"),
     path("redactors/", RedactorListView.as_view(), name="redactor-list"),
-    path("redactors/<int:pk>", RedactorDetailView.as_view(), name="redactor-detail")
+    path("redactors/<int:pk>", RedactorDetailView.as_view(), name="redactor-detail"),
+    path("redactors/create", RedactorCreateView.as_view(), name="redactor-create"),
 
 
 ]
